ca3a16e2914294fa3cf6afd67ba400c2e03a12a7
JHdelacrus <132547829+JHdelacrus@users.noreply.github.com>
2023-06-02 12:38:40 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,16 @@
-
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "POWER") {
+        lista = [0, 1]
+    }
+})
+let lista: number[] = []
+radio.setGroup(1)
+let encendido = false
+led.enable(encendido)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
